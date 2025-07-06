8329f3a983e24b917628bfd784b0065ea86abba9
Laura-Moya <l.moya@alumnos.upm.es>
2025-07-06 20:44:22 +0200

    Falta arreglar lo de las fotos

--- src/app/pages/technology/technology.component.ts
+++ src/app/pages/technology/technology.component.ts
@@ -17,14 +17,14 @@ launches = [
     },
     {
       name: 'SPACEPORT',
-      image: 'assets/crew/image-mark-shuttleworth.png',
+      image: 'assets/technology/image-spaceport-landscape.jpg',
       description: `A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport 
       for aircraft. Based in the famous Cape Canaveral, our spaceport is 
       ideally situated to take advantage of the Earth's rotation for launch.`,
     },
     {
       name: 'SPACE CAPSULE',
-      image: 'assets/crew/image-victor-glover.png',
+      image: 'assets/technology/image-space-capsule-landscape.jpg',
       description: `A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without 
       wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep 
       you entertained.`,
